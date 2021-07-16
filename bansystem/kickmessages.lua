24f36aabfdbca2252ff81fc948d90624665027a2
soop <77305497+th3n0y0u@users.noreply.github.com>
2021-07-16 15:11:15 -0700

    Create kickmessages.lua

---
+++ bansystem/kickmessages.lua
@@ -0,0 +1,24 @@
+local module = {
+	"loser imagine hacking in a parkour game",
+	"get a life you hobo you spent 20$ on hacks just to be kicked by a script",
+	"ERROR MESSAGE 404",
+	"You have been banned. Reason: Loser, Ban Length: 10000000000000000000000000000000000 hours",
+	"Please rejoin if you think this is a obvious joke that your 20$ hacks don't work",
+	"nice try nerd",
+	"you can't code(unless you made your hacks then respect to you but it's kinda bad)",
+	"????",
+	"big loser",
+	"minceraft",
+	"funny huh?",
+	"desperate to beat a person on the leaderboards I see",
+	"no",
+	"you big loser",
+	"ratio",
+	"yes this is supposed to be dehumanizing for cheaters like you",
+	"wassup cheater I see you are back",
+	"lol no",
+	"You have been banned. Reason: Bruh, Ban Length: 1000000000000000000000000000000000000000000000000000000000000000000000000000000 seconds",
+	"shush"
+}
+
+return module
